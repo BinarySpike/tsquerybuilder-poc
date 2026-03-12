b7cafbf62f547d9045509073501c692789ae1f1a
Nathan Goings <BinarySpike@gmail.com>
2026-03-12 09:17:10 -0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-import type { QueryBuilder } from './testheavy'
+import type { QueryBuilder } from './testheavy.ts'
 
 let qb = {} as QueryBuilder;
 
@@ -9,11 +9,24 @@ qb.where().is().and.is()
 qb.where().is().and.not.is()
 qb.where().is().select()
 qb.where().not.is().select()
+qb.where().is().and.is().and.is().or.is().select()
 qb.where().is().and.is().select()
 qb.where().is().and.not.is().select()
 qb.where().is().andWhere().not.is().select()
+qb.where().is().and.not.is().andWhere().is().select()
+qb.where().is().and.not.is().andWhere(i => i.where().is().and.is()).select()
+qb.where().is().and.is().andWhere(i => i.where().is().or.is().and.is()).select()
+qb.where(i => i.where().is()).and.is().and.is()
+qb.where(i => i.where().is()).andWhere(i => i.where().is()).and.is()
 qb.where().is().andWhere().is().and.not.is().select()
 qb.where(i => i.where().is())
-qb.where(i => i.where().not.is()).select()
+qb.where(i => i.where().not.is())
 qb.where().is().andWhere(i => i.where().is()).select()
 qb.where(i => i.where().is().andWhere().is()).select()
+
+qb.where(a => a.where(b => b.where().is()))
+
+qb.where().not.is().and.not.is().and.not.is().and.not.is().and.not.is().and.not.is().and.not.is().and.not.is().and.not.is().and.not.is().and.not.is().and.not.is().and.not.is().and.not.is()
+
+// qb.where(i => i.where().is().select())
+// qb.where(i => i.where().is().and.is().select())
